b656babc66620527891d18e32549a9a70db84b6b
Trial and Errror <coding.trialanderrror@gmail.com>
2020-12-20 12:22:46 -0500

    Added performance timer

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 from src.fetcher.fetch import fetch
+import time
+
+
+def fetch_with_timer():
+    """
+    Start and stop timer for fetch; print results on console.
+
+    :return: None
+    """
+    start = time.perf_counter()
+    fetch()
+    stop = time.perf_counter()
+    print(f'\n\nDownloaded videos in {stop-start:0.4f} seconds')
+
 
 if __name__ == '__main__':
-    fetch()
+    fetch_with_timer()
